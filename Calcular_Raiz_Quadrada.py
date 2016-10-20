29de2047249ddffc1dfd103a8723a5e7f2f4f803
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-19 23:31:38 -0200

    Update Calcular_Raiz_Quadrada.py

--- Calcular_Raiz_Quadrada.py
+++ Calcular_Raiz_Quadrada.py
@@ -4,13 +4,13 @@
 #Date: 10/19/2016 U.S.A
 
 
-import math
+import math                 #Importa o modulo (math) Matematica
 
 ### Programa para saber raiz quadrada!
 
-raiz_resposta = float(input('Digite o numero (x): '))
-print(type(raiz_resposta))
-resposta = float(math.sqrt(raiz_resposta))
-print(type(resposta))
-print('Valor inserido: ' + str(raiz_resposta))
-print('Valor da Raiz: ' + str(resposta))
+raiz_resposta = float(input('Digite o numero (x): '))   #Valor a ser inserido pelo usuario! Convertendo em ponto flutuante a entrada
+print(type(raiz_resposta))                              #Saida usando a funcao type para saber o tipo da variavel inserida.
+resposta = float(math.sqrt(raiz_resposta))              #Calculo usando o modulo math, com a funcao sqrt para calculo de Raiz.
+print(type(resposta))                                   #Saida usando novamente a funcao type, para saber o tipo.
+print('Valor inserido: ' + str(raiz_resposta))          #Saida valor inserido pelo usuario.
+print('Valor da Raiz: ' + str(resposta))                #Saida calculado a raiz quadrada da entrada do usuario.
